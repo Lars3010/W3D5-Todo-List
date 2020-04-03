46fe172f4a53fec11046fad0e31e417cfe73e90e
Lars Roovers <larsroovers@gmail.com>
2020-04-03 17:11:13 +0200

    added delete task function

--- api-client.js
+++ api-client.js
@@ -1,11 +1,11 @@
-const endpointUrl = `https://wincacademydatabase.firebaseio.com/lars/tasks.json`;
+const endpointUrl = `https://wincacademydatabase.firebaseio.com/lars/tasks`;
 
 /**
  * Function that makes a fetch call to the endpoint and gets all tasks that are stored there. Converts the response into json and returns json.
  */
 const getAllTasks = async () => {
     try {
-        const response = await fetch(endpointUrl, {method: 'GET'});
+        const response = await fetch(`${endpointUrl}.json`, {method: 'GET'});
         const json = await response.json();
         return json;
     } catch (error) {
@@ -21,8 +21,16 @@ const getAllTasks = async () => {
  */
 const postTask = async(task) => {
     try {
-        const post = await fetch(endpointUrl, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(task) });
+        const post = await fetch(`${endpointUrl}.json`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(task) });
         return post;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteTask = async(hash) => {
+    try {
+        const deleteItem = await fetch(`${endpointUrl}/${hash}.json`, {method: 'DELETE'});
     } catch (error) {
         console.log(error);
     }
