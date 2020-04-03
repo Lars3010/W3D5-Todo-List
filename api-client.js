5259c91f3db8987caebd6e7811ca7faad82f69cc
Lars Roovers <larsroovers@gmail.com>
2020-04-03 15:52:41 +0200

    added post function to add tasks to database

--- api-client.js
+++ api-client.js
@@ -4,9 +4,21 @@ const endpointUrl = `https://wincacademydatabase.firebaseio.com/lars/tasks.json`
  * Function that makes a fetch call to the endpoint and gets all tasks that are stored there. Converts the response into json and returns json.
  */
 const getAllTasks = async () => {
-    const response = await fetch(endpointUrl, {method: 'GET'});
-    const json = await response.json();
-    return json;
+    try {
+        const response = await fetch(endpointUrl, {method: 'GET'});
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-//getAllTasks();
+const postTask = async(task) => {
+    try {
+        const post = await fetch(endpointUrl, {method: 'GET', headers: {'Content-Type': 'application/json'} });
+        return post;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
